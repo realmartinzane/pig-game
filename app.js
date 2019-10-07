bcd2504ecb9ce329f21fa3e736b3b99cb2f9cc62
Martin Zane <realmartinzane@gmail.com>
2019-10-07 15:02:13 +0200

    update player and change the active player

--- app.js
+++ app.js
@@ -24,7 +24,27 @@ document.getElementById('current-1').textContent = '0';
 document.querySelector('.btn-roll').addEventListener('click', function()
 {
     var dice = Math.floor(Math.random() * 6) + 1;
+
     var diceDOM = document.querySelector('.dice');
     diceDOM.style.display = 'block';
     diceDOM.src = 'dice-' + dice + '.png';
+
+    if (dice != 1)
+    {
+        roundScore += dice;
+        document.getElementById('current-' + activePlayer).textContent = roundScore;
+    }
+    else 
+    {
+        activePlayer = activePlayer === 0 ? 1 : 0;
+        roundScore = 0;
+
+        document.getElementById('current-0').textContent = '0';
+        document.getElementById('current-1').textContent = '0';
+        
+        document.querySelector('.player-0-panel').classList.toggle('active');
+        document.querySelector('.player-1-panel').classList.toggle('active');
+
+        document.querySelector('.dice').style.display = 'none';
+    }
 });
